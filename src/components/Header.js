28e0932be6bc02e3f2371b2f627f24a6d41cc3ff
Matheus Gomes <matheusgomes192@hotmail.com>
2021-02-17 20:08:59 -0300

    Terminando o Header + usando @keyframes animation na imagem e textos para dar efeito de fade ao trocar o Carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
+import {useState} from "react";
 import './Header.css';
+
 import Header1 from '../img/header.png';
+import Header2 from '../img/header2.png';
+
 import {withStyles} from "@material-ui/core/styles";
 import {Button} from "@material-ui/core";
 
 export default () => {
+    const [header, setHeader] = useState(true);
+
     const DefaultBtn = withStyles(() => ({
         root: {
             padding: 20,
@@ -27,17 +33,46 @@ export default () => {
         },
     }))(Button);
 
+
     return (
         <div className={"header"}>
+            <div className={"btnCarousel"}>
+                <div style={{backgroundColor: header ? '#CD553F' : '#999'}} onClick={() => setHeader(!header)}
+                     className={"ball"}>1
+                </div>
+                <div style={{backgroundColor: header ? '#999' : '#CD553F'}} onClick={() => setHeader(!header)}
+                     className={"ball"}>2
+                </div>
+            </div>
+
             <div className={"left"}>
                 <span className={"topText"}>Latest New Design 2020</span>
-                <span className={"centerText"}>Fashion Lookbook</span>
-                <span className={"bottomText"}>
-                    combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable
-                </span>
+                {header ?
+                    <>
+                        <span className={"centerText"}>Fashion Lookbook</span>
+                        <span className={"bottomText"}>
+                            combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
+                            reasonable
+                        </span>
+                    </>
+                    :
+                    <>
+                        <span className={"centerText2"}>Fashion Lookbook</span>
+                        <span className={"bottomText2"}>
+                            combined with a handful of model sentence structures, to generate Lorem Ipsum which looks
+                            reasonable
+                        </span>
+                    </>
+                }
+
                 <DefaultBtn>Discover now</DefaultBtn>
             </div>
-            <img className={"img"} src={Header1}/>
+
+            {header ?
+                <div className={"img"}></div>
+                :
+                <div className={"img2"}></div>
+            }
         </div>
     )
 }
