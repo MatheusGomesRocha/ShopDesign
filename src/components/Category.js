aab0384e8eee95c9cdc4ebcf96c073dfb82e42ea
Matheus Gomes <matheusgomes192@hotmail.com>
2021-02-19 11:39:08 -0300

    Finalizando Frontend do Component Category + passando os .svg para .js para conseguir usar as props e mudar de cor ao realizar um hover

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,28 +1,42 @@
 import {useState} from "react";
 import './Category.css';
+import ScrollContainer from 'react-indiana-drag-scroll'
 
 import LampIcon from '../svg/lamp';
 import ShirtIcon from '../svg/shirts';
 import ShoeIcon from '../svg/shoes';
 import CameraIcon from '../svg/camera';
+import GlassIcon from '../svg/oculos';
+import CouchIcon from '../svg/sofa';
+import BagIcon from '../svg/shopping-bag';
+import WatchIcon from '../svg/watch';
 
 export default () => {
     const [hoverItem, setHoverItem] = useState(false);
     const [hoverItem2, setHoverItem2] = useState(false);
     const [hoverItem3, setHoverItem3] = useState(false);
     const [hoverItem4, setHoverItem4] = useState(false);
+    const [hoverItem5, setHoverItem5] = useState(false);
+    const [hoverItem6, setHoverItem6] = useState(false);
+    const [hoverItem7, setHoverItem7] = useState(false);
+    const [hoverItem8, setHoverItem8] = useState(false);
 
     let array = [
         {id: 1, text: 'jodhpur pants', Icon: LampIcon, hover: hoverItem, setHover: setHoverItem},
         {id: 2, text: 'mens wear', Icon: ShirtIcon, hover: hoverItem2, setHover: setHoverItem2},
         {id: 3, text: 'ice skates', Icon: ShoeIcon, hover: hoverItem3, setHover: setHoverItem3},
         {id: 4, text: 'dungarees', Icon: CameraIcon, hover: hoverItem4, setHover: setHoverItem4},
+        {id: 5, text: 'jodhpur pants', Icon: GlassIcon, hover: hoverItem5, setHover: setHoverItem5},
+        {id: 6, text: 'stirrup pants', Icon: CouchIcon, hover: hoverItem6, setHover: setHoverItem6},
+        {id: 7, text: 'footwear', Icon: BagIcon, hover: hoverItem7, setHover: setHoverItem7},
+        {id: 8, text: 'punk pants', Icon: WatchIcon, hover: hoverItem8, setHover: setHoverItem8},
     ]
 
     return(
         <div className={"category"}>
             <span className={"title"}>Our Category</span>
             <div className={"rowDiv"}>
+                <ScrollContainer style={{display: 'flex'}} className="scroll-container">
                 {array.map((item, k) => (
                     <div className={"itemDiv"}>
                         <div className={"itemDashed"} onMouseOver={() => item.setHover(true)} onMouseOut={() => item.setHover(false)}>
@@ -33,6 +47,7 @@ export default () => {
                         <span className={"text"}>{item.text}</span>
                     </div>
                 ))}
+                </ScrollContainer>
             </div>
         </div>
     )
